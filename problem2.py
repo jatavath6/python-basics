22f5e81564124d5c28a1d07b15486a81bbda30ef
jatavath6 <jatavathanusha6@gmail.com>
2026-01-09 00:30:01 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,10 @@
+marks=int(input("enter the student marks"))
+if marks>=90:
+    print("exallent student")
+
+elif marks>=74:
+    print("the good student")
+elif marks>=50:
+    print("the average student")
+else:
+    print("the below avarage student")
